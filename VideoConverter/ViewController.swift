3af34e1c2398f2adab139255f7cd1ef87e98df66
Owen Henley <owenhenley@me.com>
2019-10-07 08:23:06 +0100

    start of project

---
+++ VideoConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoConverter
+//
+//  Created by Owen Henley on 07/10/2019.
+//  Copyright © 2019 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
